b047ee635a46d8115b61d311db58005e43047724
felipe493 <felipelinhares628@gmail.com>
2025-10-27 09:17:46 -0300

    Tipos Primitivos e Saída de Dados

---
+++ aula/aula06a.py
@@ -0,0 +1,6 @@
+n1 = int(input('digite um numero: '))
+n2 = int(input('digite mais um número: '))
+s = n1 + n2
+print('A soma entre {} é {} é {}'.format(n1, n2, s))
+# outra forma 
+# print('A soma entre ', n1 'é', n2, 'é', s )
