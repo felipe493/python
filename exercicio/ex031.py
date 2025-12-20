400fba2d5beb57ff06435b32ed055c0898388d12
felipe493 <felipelinhares628@gmail.com>
2025-12-20 08:46:50 -0300

    Custo da Viagem

---
+++ exercicio/ex031.py
@@ -0,0 +1,13 @@
+distancia = float(input('Qual é a distância da sua viagem? '))
+print('Você está prestes a começar uma viagem de {}Km.'.format(distancia))
+if distancia <= 200:
+    preco = distancia * 0.50
+else:
+    preco = distancia * 0.45
+print('É o preço da sua passagem será de R${:.2f}'.format(preco))
+
+#outra forma
+'''distancia = float(input('Qual é a distância da sua viagem? '))
+print('Você está prestes a começar uma viagem de {}Km.'.format(distancia))
+preco = distancia * 0.50 if distancia <= 200 else distancia * 0.45
+print('É o preço da sua passagem será de R${:.2f}'.format(preco))'''
