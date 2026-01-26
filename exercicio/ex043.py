bb09a75c8175ca7b763af70ed1f86a6b6acf5292
felipe493 <felipelinhares628@gmail.com>
2026-01-26 09:20:10 -0300

    Índice de Massa Corporal

---
+++ exercicio/ex043.py
@@ -0,0 +1,16 @@
+peso = int(input('Qual é seu peso? (Kg) '))
+altura = float(input('Qual a sua altura? (m) '))
+imc = peso / (altura ** 2)
+print('O IMC dessa pessoa é de {:.1f}'.format(imc))
+if imc < 18.5:
+    print('Você está ABAIXO DO PESO normal')
+elif 18.5 <= imc < 25:
+    print('PARABÉNS, você está na faixa de PESO NORMAL')
+elif 25 <= imc < 30:
+    print('Você está em EXCESSO DE PESO')
+elif 30 <= imc < 35:
+    print('Você está em OBESIDADE (GRAU I)')
+elif 35 <= imc < 40:
+    print('Você está em OBESIDADE (GRAU II)')
+elif imc >= 40:
+    print('Você está em OBESIDADE (GRAU III)')
