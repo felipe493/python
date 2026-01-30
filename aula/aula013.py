a01fa7967a234c29d0771f5b6e6d250753153362
felipe493 <felipelinhares628@gmail.com>
2026-01-30 09:28:22 -0300

    Estrutura de repetição for

---
+++ aula/aula013.py
@@ -0,0 +1,45 @@
+#soma os números
+s = 0
+for c in range(0, 4):
+    n = int(input('Digite um valor: '))
+    s += n
+print('O somatóri de todos os números foi {}.'.format(s))
+
+#ler o input 10 vezes ou quanto a pessoa quiser
+'''for c in range(0, 10):
+    n = int(input('Digitw um número: '))
+print('FIM')'''
+
+#outra formar de pular de número em número
+'''i = int(input('Início: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+for c in range(i, f+1, p):
+    print(c)
+print('FIM')'''
+
+#conta do zero até o número que a pessoa digitou
+'''n = int(input('Digite um número: '))
+for c in range(0, n+1):
+    print(c)
+print('FIM')'''
+
+#pula de dois em dois até 6
+'''for c in range(0, 7, 2):
+    print(c)
+print('FIM')'''
+
+#conta do 6 até 1
+'''for c in range(6, 0, -1):
+    print(c)
+print('FIM')'''
+
+#conta de 1 até 6
+'''for c in range(1, 7):
+    print(c)
+print('FIM')'''
+
+#conta de 1 até 5
+'''for c in range(1, 6):
+    print(c)
+print('FIM')'''
